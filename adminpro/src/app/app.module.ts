f7fe031a59bcdf7647c8a8d3ced571d6f52eb624
Marcos <deandrademarcos@gmail.com>
2019-05-28 16:42:14 -0400

    Modificaciones en los modulos

--- adminpro/src/app/app.module.ts
+++ adminpro/src/app/app.module.ts
@@ -1,33 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-// modulos
+// modulos personalizados
+import { PagesModule } from './pages/pages.module';
+
+// rutas
+import { APP_ROUTES } from './app.routes';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { APP_ROUTES } from './app.routes';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    ProgressComponent,
-    PagesComponent,
     RegisterComponent
   ],
 
@@ -35,7 +24,8 @@ import { RegisterComponent } from './login/register.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- adminpro/src/app/app.routes.ts
+++ adminpro/src/app/app.routes.ts
@@ -1,34 +1,12 @@
-/*
-    Implementacion de las rutas de la aplicacion (forma manual)
-
-    Se crea el modulo de rutas y se importa los metodos de Routes de angular
-    Se declara una constante que va a alojar todas de las rutas de la aplicacion en formato JSON.
-*/
-
 import { RouterModule, Routes } from '@angular/router';
 
 // componentes
-import { PagesComponent } from './pages/pages.component';
-
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 import { RegisterComponent } from './login/register.component';
 
 // rutas
 const appRoutes: Routes = [
-    {
-        path : '',
-        component : PagesComponent,
-        children: [ // rutas hijas
-            { path : 'dashboard', component : DashboardComponent },
-            { path: 'progress', component: ProgressComponent },
-            { path: 'graficas1', component: Graficas1Component },
-            { path: '', redirectTo: '/dashboard', pathMatch: 'full' } // cuando no existe ninguna ruta va a redireccionar.
-        ]
-    },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: '**', component: NopagefoundComponent } // esta ruta se ejecuta si el usuario ingresa una URL invalida.
