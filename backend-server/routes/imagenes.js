0f59b9eed5efb34a3b1a79d483f7b79a52e748fa
Marcos <deandrademarcos@gmail.com>
2019-06-07 14:33:15 -0400

    Seccion 11 terminada - CRUD - Imagenes

--- backend-server/routes/imagenes.js
+++ backend-server/routes/imagenes.js
@@ -4,23 +4,26 @@ var app = express();
 
 // este path ya viene con node
 const path = require('path');
+const fs = require('fs');
 
 // rutas
 app.get('/:tipo/:img', (request, response, next) => {
 
     var tipo = request.params.tipo;
-    var imagen = request.params.img;
+    var img = request.params.img;
 
     // __dirname permite especificar toda la ruta del archivo
-    var pathImagen = path.resolve( __dirname, `` );
+    // el segundo parametro es lo que se quiere concatenar a dirname
+    var pathImagen = path.resolve( __dirname, `./../uploads/${ tipo }/${ img }`);
 
-
-
-    // OK => peticiones correctas
-    response.status(200).json({
-        ok: true,
-        mensaje: 'peticion realizada correctamente'
-    }); 
+    // verificar si la imagen existe
+    if (fs.existsSync(pathImagen)) {
+        response.sendFile( pathImagen );
+    }
+    else {
+        var pathNoImage = path.resolve( __dirname, './../assets/no-img.jpg');
+        response.sendFile(pathNoImage);
+    }
 });
 
 module.exports = app;
